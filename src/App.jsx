3081b5598eeb95d5e1ad62ab376c5b24a4522395
Aman Singh <singhaman@gmail.com>
2025-07-12 18:04:06 +0530

    landing page work add new section

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import OurExperties from "./Pages/OurExperties";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (
