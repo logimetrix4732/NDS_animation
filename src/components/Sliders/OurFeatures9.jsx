462f5369bb9e78bf523310cb8929a2a2e934c4a1
Aman Singh <singhaman@gmail.com>
2025-08-25 18:34:04 +0530

    new code per

--- src/components/Sliders/OurFeatures9.jsx
+++ src/components/Sliders/OurFeatures9.jsx
@@ -41,8 +41,7 @@ const slideData = [
   {
     img: carImg1,
     title: "Cost Efficiency",
-    subtitle:
-      "Reduce your IT overhead with our cost effective cloud solutions.",
+    subtitle: "Reduce your IT overhead with our cost effective cloud.",
   },
   {
     img: carImg9,
