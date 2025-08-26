91620fea688bdbfb8f7708389f7c6b98d5acd1f5
Aman Singh <singhaman@gmail.com>
2025-08-26 18:34:50 +0530

    new changes 1

--- src/App.jsx
+++ src/App.jsx
@@ -150,23 +150,25 @@ const App = () => {
         <Route path="*" element={<Errors />} />
       </Routes>
       {!isAdminRoute && <Footer />}
-      <div className="scroll-top">
-        <svg
-          className="progress-circle svg-content"
-          width="100%"
-          height="100%"
-          viewBox="-1 -1 102 102"
-        >
-          <path
-            d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
-            style={{
-              transition: "stroke-dashoffset 10ms linear 0s",
-              strokeDasharray: "307.919, 307.919",
-              strokeDashoffset: "307.919",
-            }}
-          />
-        </svg>
-      </div>
+      {!isAdminRoute && (
+        <div className="scroll-top">
+          <svg
+            className="progress-circle svg-content"
+            width="100%"
+            height="100%"
+            viewBox="-1 -1 102 102"
+          >
+            <path
+              d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
+              style={{
+                transition: "stroke-dashoffset 10ms linear 0s",
+                strokeDasharray: "307.919, 307.919",
+                strokeDashoffset: "307.919",
+              }}
+            />
+          </svg>
+        </div>
+      )}
     </>
   );
 };
